cd44cf5fe7230e71d041732a5cedb11fd3c13e30
sboy21 <khvedelidzesaba507@gmail.com>
2024-05-12 12:34:43 +0400

    14 commit

---
+++ Day 12/homework.py
@@ -0,0 +1,28 @@
+for i in range (1,10,2):
+   print(i)
+
+for i in range (1,50 + 1):
+     if i % 5 == 0:
+      print (i)
+
+end = int(input("Please enter whole number: "))
+
+for i in range(0, end + 1):
+    print(i)
+
+result = 0
+
+for i in range(50):
+    print(i)
+    result = result + i
+
+print(result)
+
+
+
+
+
+
+
+
+
---
+++ Day 14/homework.py
@@ -0,0 +1,8 @@
+num1 = input("please enter first number:")
+
+for i in range(0, 50):
+
+ while i % 5:
+   print(i)
+
+i += 1
