d237f76597047f9f2be4f7511b1db00e21b4ba61
sboy21 <khvedelidzesaba507@gmail.com>
2024-06-08 22:27:22 +0400

    19 commit

---
+++ Day 19/classswork.py
@@ -0,0 +1,34 @@
+
+films = ["harry potter", "trasformers", "Godzila", "Willy Wonka", "predator","smurfs","in and out"]
+
+slice(3,6)
+
+print(films[1:4])
+
+cars = ["Pagani", "bmw", "Mercdes", "Ferari", "Luka", "Lile", "Nia", "Xiamoi", "SAQW", "YOOO"]
+
+print(cars[-1])
+print(cars[-2])
+print(cars[-3])
+
+print(cars[0:3]) # 0 1 2
+
+print(cars[3:10:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
